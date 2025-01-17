8bcc08761dbbea86511df2fb9ca69628bbab70e3
ZenitsuV <git@stackblitz.com>
2025-01-17 17:44:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nodejs-express-mongodb-simple-ecommerce
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ZenitsuV/nodejs-express-mongodb-simple-ecommerce)
---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://Vignesh:P7EMppitrJIzOPfi@cluster0.rfy2f3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then((client) => {
+      console.log('Connected');
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
